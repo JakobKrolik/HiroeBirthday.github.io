3ec819761c11db5ae3e80ae7118e3433baa2c1df
HiroeBirthday <threshere@gmail.com>
2024-05-07 22:52:35 -0400

    added files
    Added files

---
+++ Controllers/SlidePuzzleController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HiroeBirthdayGift.Controllers
+{
+    public class SlidePuzzleController : Controller
+    {
+        public IActionResult Dog()
+        {
+            return View();
+        }
+        public IActionResult Pochacco()
+        {
+            return View();
+        }
+        public IActionResult Game()
+        {
+            return View();
+        }
+    }
+}
