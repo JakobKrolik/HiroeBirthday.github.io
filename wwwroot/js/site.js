fe5dca0572f66cc098ce803c1dbdf240e921f18d
HiroeBirthday <threshere@gmail.com>
2024-05-11 14:56:58 -0400

    worked more, made gif, working on font

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,34 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const player = document.getElementById('backgroundMusic');
+    const volumeIcon = document.getElementById('volumeIcon');
+    const volumeSlider = document.getElementById('volumeControl');
 
-// Write your JavaScript code.
+    // Set initial volume and icon
+    volumeSlider.value = parseFloat(sessionStorage.getItem('volume') || volumeSlider.value);
+    player.volume = parseFloat(volumeSlider.value);
+    updateVolumeIcon(player.volume);
+
+    volumeSlider.addEventListener('input', function () {
+        const volume = parseFloat(this.value);
+        player.volume = volume;
+        sessionStorage.setItem('volume', volume);
+        updateVolumeIcon(volume);
+    });
+
+    function updateVolumeIcon(volume) {
+        if (volume === 0) {
+            volumeIcon.className = 'fas fa-volume-mute text-white';
+        } else if (volume > 0.5) {
+            volumeIcon.className = 'fas fa-volume-up text-white';
+        } else {
+            volumeIcon.className = 'fas fa-volume-down text-white';
+        }
+    }
+    // Restore music time and attempt to play
+    player.currentTime = parseFloat(sessionStorage.getItem('musicTime') || 0);
+    player.play(); // Consider browser autoplay policy
+
+    window.addEventListener('beforeunload', function () {
+        sessionStorage.setItem('musicTime', player.currentTime);
+    });
+});
